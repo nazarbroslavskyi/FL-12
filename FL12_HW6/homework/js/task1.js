20f34172ca9c9531bdd43998e38a639519147b85
nazya <nazarbroslaw>
2019-12-24 23:51:34 +0200

    done

--- FL12_HW6/homework/js/task1.js
+++ FL12_HW6/homework/js/task1.js
@@ -1,1 +1,30 @@
-// Your code goes here
+function isNumber(n) {
+	return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+function solutOfQuadratEquat() {
+	const a = prompt('Please enter a', '');
+	const b = prompt('Please enter b', '');
+	const c = prompt('Please enter c', '');
+	let x1, x2, ValueOFDisc, x;
+    const number = 4;
+    const numberOfTwo = 2;
+	if(isNumber(a) && isNumber(b) && isNumber(c) && +a !== 0) {
+		ValueOFDisc = b * b - number * a * c;
+	} else {
+		return 'Invalid input data';
+	}
+
+	if(ValueOFDisc > 0) {
+		x1 = (-b + Math.sqrt(ValueOFDisc)) / (numberOfTwo * a);
+		x2 = (-b - Math.sqrt(ValueOFDisc)) / (numberOfTwo * a);
+		return 'x1 = ' + x1 + '\n' + 'x2 = ' + x2;
+	} else if(ValueOFDisc === 0) {
+		x = -b / (numberOfTwo * a);
+		return 'x = ' + x;
+	} else if(ValueOFDisc < 0) {
+		return 'no solution';
+	}
+}
+
+alert(solutOfQuadratEquat());
--- FL12_HW6/homework/js/task2.js
+++ FL12_HW6/homework/js/task2.js
@@ -1,1 +1,26 @@
-// Your code goes here
+let firstSide = parseFloat(+prompt('Enter first side of triangle ',''));
+let secondSide = parseFloat(+prompt('Enter second side of triangle ',''));
+let thirdSide = parseFloat(+prompt('Enter third side of triangle ',''));
+
+if(firstSide <= 0 || secondSide <= 0 || thirdSide <= 0) {
+
+    console.log('Triangle doesn’t exist');
+} else if(firstSide + secondSide > thirdSide
+    && secondSide + thirdSide > firstSide
+    && thirdSide + firstSide > secondSide) {
+
+    if(firstSide === secondSide && firstSide !== thirdSide
+    || secondSide === thirdSide && thirdSide !== firstSide
+    || thirdSide === firstSide && secondSide !== firstSide) {
+        console.log('Isosceles triangle');
+    } else if(firstSide === secondSide
+            && secondSide === thirdSide
+            && thirdSide === firstSide) {
+            console.log('Euquivalent triangle')
+    } else {
+        console.log('Normal triangle');
+    }
+
+} else {
+    console.log('Triangle doesn’t exist');
+}
