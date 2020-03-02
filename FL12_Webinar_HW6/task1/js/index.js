0cf09f43b8231d871c6a5a0c4bb7f87a084387b7
Nazar Broslavskyi <bnmstdkpi@gmail.com>
2020-03-02 15:48:45 +0200

    initial commit

---
+++ FL12_Webinar_HW6/task1/js/index.js
@@ -0,0 +1,80 @@
+// $("#someId").css({"prop" : "value", "prop1" : "value1", ...});
+
+//body: add padding 20px
+
+// id title: make align center, remove top margin
+
+// div after header: add double border and padding 20px
+
+// change color for all h2 and set top margin to 0
+
+// set font size 18px for all .list direct child
+
+
+
+// ******************************************************
+// :even, :odd, :first, :last, :not(), :empty
+// :gt() /* all elements at an index greater then specified */
+// :lt() /* all elements at an index less then specified */
+// :hidden /* display: none, type="hidden", width & height = 0, ancestor is hidden */
+// :visible /* are visible */
+// :parent /* are parents to other elements, including text node */
+// :contains() /* contain the specified text */
+// :has() /* contain at least one element that matches the specified selector */
+// ******************************************************
+
+// in #list-1
+  // show all hidden and not cloned li
+
+  // hide empty li
+
+// in #list-3
+  // for all even li set margin-left -20px
+
+  // for the first li set any different color
+
+  // for all li with index > 5 set color to #ccc
+
+// show ul which is parent
+
+// for li wich has 'em' add red color
+
+// for li which contains text 'Buratino' set font weight to bold
+
+
+
+
+// ******************************************************
+// :first-child, :last-child, :only-child, :first-of-type, :last-of-type,
+// :only-of-type, :nth-child(), :nth-last-child(), :nth-last-of-type(),
+// :nth-of-type()
+// ******************************************************
+
+// for b in p which is the only child set font size 36px
+
+// for em in p which is the last child of type set color to green
+
+
+
+
+// ******************************************************
+// [name], [name|='value'], [name*='value'], [name~='value'],
+// [name$='value'], [name^='value']
+// [name!='value'] /* neither such attribute nor specified value */
+// ******************************************************
+
+// set width 80px for input with attribute name ended by 'age'
+// set width 120px for input with attribute name started by 'my'
+// console.log checked checkbox
+// show all images with a cat
+
+
+// ******************************************************
+// is(), not(), has()
+// eq(), first(), last()
+// find(), parent(), parents(), closest()
+// children(), prev(), next(), siblings()
+// ******************************************************
+// for .mbox with index 3 set padding-top 50px
+// for first div wraper for img set float left and border red
+
